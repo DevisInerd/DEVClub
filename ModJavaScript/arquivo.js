0a18745a310c7aaf0c6eac074e24143844371212
DevisInerd <inerd.itad@gmail.com>
2026-01-25 12:57:41 -0400

    cont

--- ModJavaScript/arquivo.js
+++ ModJavaScript/arquivo.js
@@ -1,2 +1,4 @@
-const input = document.getElementById("main-input")
-console.log(input)
+const elements = document.querySelector("paragraph-js")
+console.log(elements.textContent) //SÓ HTML
+console.log(elements.innerText) // Leva em conta o CSS
+console.log(elements.innerHTML) // Trás tudo
